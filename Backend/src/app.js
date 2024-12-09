57c079c5722333d064d375fa1cfc5821c0d679e0
Naveen K <naveennavi1711@gmail.com>
2024-12-09 08:36:11 +0530

    exploring the routing

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,8 +2,26 @@ const express=require('express');
 
 const app= express();
 
-app.use("/test",(req,res)=>{
-    res.send("I am gost from port 8000");
+
+
+app.get("/user",(req,res)=>{
+    res.send({
+        "firsname":"Naveen",
+        "Lastname":"K"
+    })
+});
+
+app.post("/user",(req,res)=>{
+
+    res.send("Data stored successfully in db")
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("Data deleted from the db")
+})
+
+app.use("/",(req,res)=>{
+    res.send("I am from / route")
 });
 
 app.listen(8000,()=>{
